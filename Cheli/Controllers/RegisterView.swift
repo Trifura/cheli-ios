f69891efbc13101be5374d4506b97748aee56ce3
medoz3ro <bsabo44@gmail.com>
2023-05-11 16:47:29 +0200

    Login/Register
    Register radi
    Login radi
    Landing
    Sve editano lijepo

--- Cheli/Controllers/RegisterView.swift
+++ Cheli/Controllers/RegisterView.swift
@@ -8,26 +8,25 @@
 import SwiftUI
 
 struct RegisterView: View {
-    @State private var name1: String = ""
-    @State private var username1: String = ""
-    @State private var email1: String = ""
-    @State private var pass1: String = ""
+    @State private var fullName: String = ""
+    @State private var username: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var showErrorAlert = false
     
-    
-    
+    @ObservedObject var viewModel: UserViewModel = UserViewModel()
+    @EnvironmentObject var userStore: UserStore
     
     var body: some View {
-        
         VStack{
             
             
             Text("Create an account ✏️")
-             .padding(.top, 24)
-             .modifier(HeaderTextViewModifier())
-             .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 24)
+                .modifier(HeaderTextViewModifier())
             
             
-            Text("Please enter your full name, username, email address and password. If you forget it, then you have to do forgot password.")
+            Text("Please enter your fullfullName, username, email address and password. If you forget it, then you have to do forgot password.")
                 .modifier(MarginViewModifier())
                 .padding(.top, 20)
                 .font(.system(size: 18))
@@ -37,45 +36,82 @@ struct RegisterView: View {
             
             
             Group{
-                Text("Full name")
+                Text("Full Name")
                     .modifier(FormTextViewModifier())
                     .padding(.top, 32)
-                TextField("Benjamin Sabo", text: $name1)
+                TextField("Enter your full name", text: $fullName)
                     .modifier(MarginViewModifier())
-                
-                
-                
+                Divider()
+                    .foregroundColor(Color("primary500"))
+                    .modifier(MarginViewModifier())
+            }
+            
+            Group{
                 Text("Username")
                     .modifier(FormTextViewModifier())
                     .padding(.top, 32)
-                TextField("BenjaminSabo", text: $username1)
+                TextField("Enter your username", text: $username)
                     .modifier(MarginViewModifier())
-                
+                    .autocapitalization(.none)
+                Divider()
+                    .foregroundColor(Color("primary500"))
+                    .modifier(MarginViewModifier())
+            }
+            
+            Group{
                 Text("Email")
                     .modifier(FormTextViewModifier())
                     .padding(.top, 32)
-                TextField("example@example.com", text: $email1)
+                TextField("Enter your email", text: $email)
                     .modifier(MarginViewModifier())
-                
-                
-                Text("Password")
-                    .modifier(FormTextViewModifier())
-                    .padding(.top, 32)
-                SecureField("Password", text: $pass1)
+                    .autocapitalization(.none)
+                Divider()
+                    .foregroundColor(Color("primary500"))
                     .modifier(MarginViewModifier())
             }
             
             
-            
-            Spacer()
-            
-            Button("SIGN IN") {
-             print("Button pressed")
-             }
-             .modifier(ButtonViewModifier())
-            
+            Group{
+                Text("Password")
+                    .modifier(FormTextViewModifier())
+                    .padding(.top, 32)
+                SecureField("Enter your password", text: $password)
+                    .modifier(MarginViewModifier())
+                Divider()
+                    .foregroundColor(Color("primary500"))
+                    .modifier(MarginViewModifier())
+            }
             
         }
+        
+        
+        
+        Spacer()
+        
+        Button {
+            viewModel.registerUser(fullName:fullName, username: username, email: email, password: password) { success in
+                if success {
+                    print("Register successful")
+                    userStore.setMainUser()
+                    userStore.isLogged = true
+                } else {
+                    print("Register failed")
+                    showErrorAlert = true
+                }
+            }
+        }label: {
+            Text("SIGN IN")
+                .frame(maxWidth: .infinity)
+        }
+        .modifier(ButtonViewModifier())
+        .alert(isPresented: $showErrorAlert) {
+            Alert(
+                title: Text("Register Failed"),
+                message: Text("Try again later."),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
--- Cheli/View/ViewModifiers/ButtonViewModifier.swift
+++ Cheli/View/ViewModifiers/ButtonViewModifier.swift
@@ -12,12 +12,10 @@ struct ButtonViewModifier: ViewModifier {
         content
             .frame(width: 380.0, height: 50.0)
             .background(Color("primary500"))
-            .mask {
-                RoundedRectangle(cornerRadius: 100)
-            }
-            .frame(maxWidth: .infinity)
+            .mask(RoundedRectangle(cornerRadius: 100))
             .font(.system(size: 16))
             .foregroundColor(Color("button1Color"))
+            .contentShape(Rectangle())
     }
 }
 
